dcec4a3c1ce3890491472c100f8c07749b51ceda
Kenny Pflug <kenny.pflug@live.de>
2015-01-25 14:09:46 +0100

    added class Chip

---
+++ Code/NetUserGroupRegensburg.ConnectFour/ConnectFour.Core/Chip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConnectFour.Core
+{
+    public class Chip
+    {
+        public readonly string PlayerName;
+        public readonly Color Color;
+
+        public Chip(string playerName, Color color)
+        {
+            if (playerName == null) throw new ArgumentNullException("playerName");
+            if (color == null) throw new ArgumentNullException("color");
+
+            PlayerName = playerName;
+            Color = color;
+        }
+    }
+}
