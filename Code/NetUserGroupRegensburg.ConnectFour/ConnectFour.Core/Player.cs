eb2127a0829d11f4832f3b3aa298e4415a62df02
Kenny Pflug <kenny.pflug@live.de>
2015-06-11 15:23:20 +0200

    moved IPlayer interface to own file, added BoardLine class diagram

--- Code/NetUserGroupRegensburg.ConnectFour/ConnectFour.Core/Player.cs
+++ Code/NetUserGroupRegensburg.ConnectFour/ConnectFour.Core/Player.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 
 namespace ConnectFour.Core
 {
-    public interface IPlayer
-    {
-        void PlaceChipInColumn(IColumn column);
-        string Name { get; }
-        Color Color { get; }
-        IList<Chip> Chips { get; }
-    }
-
     public class Player : IPlayer
     {
         private readonly string _name;
